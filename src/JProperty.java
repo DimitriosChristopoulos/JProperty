901403ebeb1d5cc9a59a8156f8c5985ddac551e9
Ket93 <kevincui113@gmail.com>
2021-06-11 23:18:16 -0400

    Adding MyFrame

--- src/JProperty.java
+++ src/JProperty.java
@@ -1,5 +1,6 @@
 public class JProperty {
     public static void main(String[] args){
         System.out.println("JProperty!");
+        new MyPanel();
     }
 }
---
+++ src/MyPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyPanel extends JPanel{
+
+    JButton button;
+    MyPanel(){
+        // Creating buttons
+        button = new JButton();
+        
+        this.setSize(1200,800);
+    }
+}
